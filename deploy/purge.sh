955259e437ae081a047a63e4964c55d35b5387da
Nick Aquina <nickaquina@gmail.com>
2020-10-21 14:20:25 +0200

    Add warning that daemon should be stopped before purging

--- deploy/purge.sh
+++ deploy/purge.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+echo ""
+echo "If the daemon is still running, please stop it before running this script."
+echo ""
+
 set -ex
+
 rm -f "/etc/systemd/network/90-wg0.network"
 rm -f "/etc/systemd/network/90-wg0.netdev"
 systemctl restart systemd-networkd
