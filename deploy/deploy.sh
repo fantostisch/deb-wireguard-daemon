a498793c29247916cdc79a2f251dba92261e4268
Nick Aquina <nickaquina@gmail.com>
2020-10-29 20:16:49 +0100

    Do not install iproute2 in deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,6 @@ sudo apt-get update
 sudo apt-get -y install linux-headers-"$(dpkg --print-architecture)"
 sudo apt-get -y install golang-go
 sudo apt-get install wireguard
-sudo apt-get install -y iproute2
 
 ###############################################################################
 # Keys
